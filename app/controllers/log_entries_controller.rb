07f2d994e75bcc55f61bae6244c7b9c4d5497b45
Ward-R <4577045+Ward-R@users.noreply.github.com>
2025-06-25 16:37:39 +0900

    added forms and for creating logs and log entries.

--- app/controllers/log_entries_controller.rb
+++ app/controllers/log_entries_controller.rb
@@ -3,4 +3,28 @@ class LogEntriesController < ApplicationController
   def index
     LogEntry.all
   end
+
+  def new
+    @log = Log.find(params[:log_id])
+    @log_entry = @log.log_entries.build
+    @log_entry.user = current_user
+  end
+
+  def create
+    @log = Log.find(params[:log_id])
+    @log_entry = @log.log_entries.build(log_entry_params)
+    @log_entry.user = current_user
+
+    if @log_entry.save
+      redirect_to log_path(@log), notice: 'Log Entry was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+  def log_entry_params
+    params.require(:log_entry).permit(:content, :entry_type, :section_identifier)
+  end
 end
--- app/models/log.rb
+++ app/models/log.rb
@@ -3,6 +3,8 @@ class Log < ApplicationRecord
   has_many :log_entries
 
   validates :date, presence: true
-  validates :status, presence: true
-  validates :shift_type, presence: true
+  # validates :status, presence: true
+  validates :shift_type, presence: true, inclusion: { in: ['Day Shift', 'Night Shift'], message: "must be either Day Shift or Night Shift" }
+  enum status: { open: 0, signed_off: 1, closed: 2, archived: 3 }
+  attribute :status, :integer, default: :open
 end
